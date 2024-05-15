b9445ee8b5298610fa10d67c627df2858b3287ec
Manish <2002mybaran@gmail.com>
2024-05-15 11:03:29 +0530

    Basic server

---
+++ server.js
@@ -0,0 +1,16 @@
+const { publicDecrypt } = require('crypto');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+})
+
+app.listen(8000, () => console.log('Server is running on PORT 8000'));
