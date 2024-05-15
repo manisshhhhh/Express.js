a9c2d14974402b9df0adaa231b0e735847cdf015
Manish <2002mybaran@gmail.com>
2024-05-15 11:08:04 +0530

    Static server with nodejs

--- server.js
+++ server.js
@@ -4,13 +4,8 @@ const path = require('path');
 
 const app = express();
 
+// setup static folder 
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
-
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'about.html'));
-})
 
 app.listen(8000, () => console.log('Server is running on PORT 8000'));
