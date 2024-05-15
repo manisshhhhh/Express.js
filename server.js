092841e3b1f0708fafa4b6c4d8f7b03c7e5a2e36
Manish <2002mybaran@gmail.com>
2024-05-15 11:46:14 +0530

    status codes

--- server.js
+++ server.js
@@ -20,16 +20,22 @@ app.get('/api/posts', (req, res) => {
     const limit = parseInt(req.query.limit);
 
     if (!isNaN(limit) && limit > 0) {
-        res.json(posts.slice(0, limit));
-    } else {
-        res.json(posts);
+        return res.status(200).json(posts.slice(0, limit));
     }
+    res.status(200).json(posts);
+
 })
 
 // Get a single post 
 app.get('/api/posts/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    res.json(posts.filter((post) => id === post.id));
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+        return res.status(404).json({ msg: `A Post with a ${id} not present` });
+    }
+    res.status(200).json(posts.filter((post) => id === post.id));
+
 })
 
 
